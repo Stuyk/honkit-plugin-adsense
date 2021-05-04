d7ebcaa0538cae86d0c5bc477ef32b8dfaced411
Stuyk <stuykgaming@gmail.com>
2021-05-04 07:11:32 -0600

    Fix Generation

--- index.js
+++ index.js
@@ -6,17 +6,12 @@ function handleBefore(page) {
         return page;
     }
 
-    page.content += `
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <ins class="adsbygoogle"
-            style="display:block; text-align:center;"
-            data-ad-layout="in-article"
-            data-ad-format="fluid"
-            data-ad-client="${client}"
-            data-ad-slot="${slot}"></ins>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>`;
+    page.content +=
+        '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="' +
+        client +
+        '" data-ad-slot="' +
+        slot +
+        '"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>';
 
     return page;
 }
